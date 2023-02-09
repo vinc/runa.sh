ce10012cbef3a6fe6c128f7f9caaaa3aae01386f
Vincent Ollivier <v@vinc.cc>
2023-02-09 18:57:52 +0100

    Add style to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "pages/home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
