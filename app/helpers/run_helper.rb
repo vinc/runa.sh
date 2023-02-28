57be11fabaa3c41149a97614bc1e39f87c79e73a
Vincent Ollivier <v@vinc.cc>
2023-02-28 20:05:26 +0100

    Add run helper

---
+++ app/helpers/run_helper.rb
@@ -0,0 +1,15 @@
+module RunHelper
+  def run_state(run)
+    case run.state
+    when "created" then "Queued"
+    when "started" then "Running"
+    else run.state.capitalize
+    end
+  end
+
+  def run_color(run)
+    case run.state
+    when "started" then "success"
+    end
+  end
+end
