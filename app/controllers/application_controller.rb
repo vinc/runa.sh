ce44e260b2b505099dbe517c77919c9e9f1f77ea
Vincent Ollivier <v@vinc.cc>
2023-03-04 15:45:32 +0100

    Add basic user token auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+
+  protected
+
+  def authenticate_user!
+    if request.headers["Authorization"].present?
+      authenticate_or_request_with_http_token do |token|
+        return @current_user ||= User.find_by(token: token) # FIXME: timing attack
+      end
+    end
+    super
+  end
+
+  def current_user
+    @current_user || super
+  end
+
+  def user_signed_in?
+    @current_user.present? || super
+  end
 end
