2f958672627081d5e7193e9da2c5b263be7a868b
Vincent Ollivier <v@vinc.cc>
2023-03-16 22:00:42 +0100

    Add assets to templates

--- app/mailers/run_mailer.rb
+++ app/mailers/run_mailer.rb
@@ -2,6 +2,6 @@ class RunMailer < ApplicationMailer
   def finished_email
     @run = params[:run]
     @url = ENV["RUNA_API_URL"]
-    mail(to: @run.task.user.email, subject: "#{@run.task.name} ##{@run.sequential_id}")
+    mail(to: @run.task.user.email, subject: "[Runa] #{@run.task.name} ##{@run.sequential_id}")
   end
 end
